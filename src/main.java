e47e02754c82d25738eb8c93d6c2bc60f71a6179
hopee0411 <84006813+hopee0411@users.noreply.github.com>
2021-12-30 22:05:19 +0900

    test2

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args){
-        System.out.println("Algorithm");
+        System.out.println("Algorithmd");
+        System.out.println("test");
     }
 }
