84a20296297005a31cd4eb750cd0df44ca684c14
hopee0411 <84006813+hopee0411@users.noreply.github.com>
2021-12-30 22:11:00 +0900

    add test2

--- src/main.java
+++ src/main.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+
 public class main {
     public static void main(String[] args){
         System.out.println("Algorithmd");
         System.out.println("test");
+        System.out.println("test2");
     }
 }
